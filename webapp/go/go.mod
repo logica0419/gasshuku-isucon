33b11f3dbb7b6970f0e88f6dfc1a0eee411aa568
logica0419 <84759675+logica0419@users.noreply.github.com>
2023-02-10 16:34:01 +0900

    defined domain structs

--- webapp/go/go.mod
+++ webapp/go/go.mod
@@ -2,7 +2,10 @@ module github.com/logica0419/gasshuku-isucon/webapp/go
 
 go 1.19
 
-require github.com/labstack/echo/v4 v4.10.0
+require (
+	github.com/labstack/echo/v4 v4.10.0
+	github.com/oklog/ulid/v2 v2.1.0
+)
 
 require (
 	github.com/labstack/gommon v0.4.0 // indirect
--- webapp/go/main.go
+++ webapp/go/main.go
@@ -10,11 +10,82 @@ import (
 	"os"
 	"os/exec"
 	"sync"
+	"time"
 
 	"github.com/labstack/echo/v4"
+	"github.com/oklog/ulid/v2"
 )
 
-const qrCodeFileName = "../images/qr.png"
+// ULIDを生成
+func generateID() string {
+	return ulid.Make().String()
+}
+
+// 図書館
+type Library struct {
+	ID          string `json:"id" db:"id"`
+	Name        string `json:"name" db:"name"`
+	PhoneNumber string `json:"phone_number" db:"phone_number"`
+	Address     string `json:"address" db:"address"`
+}
+
+// 会員
+type Member struct {
+	ID          string    `json:"id" db:"id"`
+	Name        string    `json:"name" db:"name"`
+	Address     string    `json:"address" db:"address"`
+	PhoneNumber string    `json:"phoneNumber" db:"phone_number"`
+	LibraryID   string    `json:"libraryId" db:"library_id"`
+	Banned      bool      `json:"banned" db:"banned"`
+	CreatedAt   time.Time `json:"created_at" db:"created_at"`
+}
+
+// 図書分類
+type Genre int
+
+// 国際十進分類法に従った図書分類
+const (
+	General         Genre = iota // 総記
+	Philosophy                   // 哲学・心理学
+	Religion                     // 宗教・神学
+	SocialScience                // 社会科学
+	Vacant                       // 未定義
+	Mathematics                  // 数学・自然科学
+	AppliedSciences              // 応用科学・医学・工学
+	Arts                         // 芸術
+	Literature                   // 言語・文学
+	Geography                    // 地理・歴史
+)
+
+// 蔵書
+type Book struct {
+	ID        string    `json:"id" db:"id"`
+	Title     string    `json:"title" db:"title"`
+	Author    string    `json:"author" db:"author"`
+	Genre     Genre     `json:"genre" db:"genre"`
+	LibraryID string    `json:"library_id" db:"library_id"`
+	CreatedAt time.Time `json:"created_at" db:"created_at"`
+}
+
+// 貸出記録
+type Lending struct {
+	ID        string    `json:"id" db:"id"`
+	UserID    string    `json:"user_id" db:"user_id"`
+	BookID    string    `json:"book_id" db:"book_id"`
+	CreatedAt time.Time `json:"created_at" db:"created_at"`
+	Due       time.Time `json:"due" db:"due"`
+}
+
+// 貸出情報付き蔵書
+type BookWithLend struct {
+	ID        string    `json:"id"`
+	Title     string    `json:"title"`
+	Author    string    `json:"author"`
+	Genre     Genre     `json:"genre"`
+	Lending   bool      `json:"lending"`
+	LibraryID string    `json:"library_id"`
+	CreatedAt time.Time `json:"created_at"`
+}
 
 var (
 	block      cipher.Block
@@ -44,6 +115,8 @@ func decrypt(cipherText string) (string, error) {
 	decryptStream.XORKeyStream(decryptedText, []byte(cipherByte[aes.BlockSize:]))
 	return string(decryptedText), nil
 }
+
+const qrCodeFileName = "../images/qr.png"
 
 // QRコードを生成
 func generateQRCode(id string) ([]byte, error) {
