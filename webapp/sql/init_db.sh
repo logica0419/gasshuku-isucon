d44fe40f3a6e10928e6df0d74700a532dee6ab7e
logica0419 <84759675+logica0419@users.noreply.github.com>
2023-02-10 16:33:35 +0900

    write db initialize script

---
+++ webapp/sql/init_db.sh
@@ -0,0 +1,10 @@
+#!/bin/bash -e
+cd "$(dirname "$0")"
+
+DB_HOST=${DB_HOST:-127.0.0.1}
+DB_PORT=${DB_PORT:-3306}
+DB_USER=${DB_USER:-isucon}
+DB_PASS=${DB_PASS:-isucon}
+DB_NAME=${DB_NAME:-isulibrary}
+
+mysql -h "$DB_HOST" -P "$DB_PORT" -u "$DB_USER" -p "$DB_PASS" "$DB_NAME" <0_schema.sql
