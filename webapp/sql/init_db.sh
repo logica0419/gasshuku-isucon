adc4688531900a1c5e8605b750bf6a33ed384f7b
logica0419 <84759675+logica0419@users.noreply.github.com>
2023-02-13 02:31:01 +0900

    fixed db init script

--- webapp/sql/init_db.sh
+++ webapp/sql/init_db.sh
@@ -7,4 +7,4 @@ DB_USER=${DB_USER:-isucon}
 DB_PASS=${DB_PASS:-isucon}
 DB_NAME=${DB_NAME:-isulibrary}
 
-cat 0_schema.sql 1_data.sql | mysql -h "$DB_HOST" -P "$DB_PORT" -u "$DB_USER" -p "$DB_PASS" "$DB_NAME"
+cat 0_schema.sql 1_data.sql | mysql -h"$DB_HOST" -P"$DB_PORT" -u"$DB_USER" -p"$DB_PASS" "$DB_NAME"
