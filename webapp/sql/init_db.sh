73d4c7ee63b65fc8febe7cbd7ef969dedc1ac925
logica0419 <84759675+logica0419@users.noreply.github.com>
2023-02-13 01:38:22 +0900

    dump data

--- webapp/sql/init_db.sh
+++ webapp/sql/init_db.sh
@@ -7,4 +7,4 @@ DB_USER=${DB_USER:-isucon}
 DB_PASS=${DB_PASS:-isucon}
 DB_NAME=${DB_NAME:-isulibrary}
 
-mysql -h "$DB_HOST" -P "$DB_PORT" -u "$DB_USER" -p "$DB_PASS" "$DB_NAME" <0_schema.sql
+cat 0_schema.sql 1_data.sql | mysql -h "$DB_HOST" -P "$DB_PORT" -u "$DB_USER" -p "$DB_PASS" "$DB_NAME"
