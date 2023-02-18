ba85548196753510ec52fea4f5ee412cf7a8a126
logica0419 <84759675+logica0419@users.noreply.github.com>
2023-02-18 13:25:06 +0900

    implement GetMembers action

--- bench/action/member.go
+++ bench/action/member.go
@@ -3,20 +3,46 @@ package action
 import (
 	"context"
 	"net/http"
+	"strconv"
 
-	"github.com/isucon/isucandar/agent"
 	"github.com/isucon/isucandar/failure"
 	"github.com/logica0419/gasshuku-isucon/bench/model"
 	"github.com/logica0419/gasshuku-isucon/bench/utils"
 )
 
-type GetMembersQueries struct {
+type GetMembersQuery struct {
 	Page         int
 	LastMemberID string
 	Order        string
 }
 
-func GetMembers(ctx context.Context, a agent.Agent) []string {}
+func (c *ActionController) GetMembers(ctx context.Context, query GetMembersQuery) (*http.Response, error) {
+	agent := c.libAgent()
+
+	url := "/api/members?"
+	if query.Page != 0 {
+		url += "page=" + strconv.Itoa(query.Page) + "&"
+	}
+	if query.LastMemberID != "" {
+		url += "last_member_id=" + query.LastMemberID + "&"
+	}
+	if query.Order != "" {
+		url += "order=" + query.Order + "&"
+	}
+	url = url[:len(url)-1] // 最後の一文字(?か&)を削除する
+
+	req, err := agent.GET(url)
+	if err != nil {
+		return nil, failure.NewError(model.ErrCritical, err)
+	}
+
+	res, err := agent.Do(ctx, req)
+	if err != nil {
+		return nil, failure.NewError(model.ErrRequestFailed, err)
+	}
+
+	return res, nil
+}
 
 type PostMemberRequest struct {
 	Name        string `json:"name"`
@@ -24,19 +50,21 @@ type PostMemberRequest struct {
 	PhoneNumber string `json:"phone_number"`
 }
 
-func PostMember(ctx context.Context, a agent.Agent, body PostMemberRequest) (*http.Response, error) {
+func (c *ActionController) PostMember(ctx context.Context, body PostMemberRequest) (*http.Response, error) {
 	reader, err := utils.StructToReader(body)
 	if err != nil {
 		return nil, failure.NewError(model.ErrCritical, err)
 	}
 
-	req, err := a.POST("/member", reader)
+	agent := c.libAgent()
+
+	req, err := agent.POST("/api/members", reader)
 	if err != nil {
 		return nil, failure.NewError(model.ErrCritical, err)
 	}
 	req.Header.Set("Content-Type", "application/json")
 
-	res, err := a.Do(ctx, req)
+	res, err := agent.Do(ctx, req)
 	if err != nil {
 		return nil, failure.NewError(model.ErrRequestFailed, err)
 	}
