59594855b0d97c5f8d6843e1f5733a430a3e33aa
logica0419 <84759675+logica0419@users.noreply.github.com>
2023-02-19 00:12:48 +0900

    implement base validator

---
+++ bench/validator/validator.go
@@ -0,0 +1,19 @@
+package validator
+
+import (
+	"net/http"
+)
+
+type ValidateOpt func(http.Response) error
+
+// http.Responseを検証
+// 渡されたoptsの順にvalidateし、どこかでValidateに失敗したらエラーを返す
+func Validate(res http.Response, opts ...ValidateOpt) error {
+	for _, opt := range opts {
+		if err := opt(res); err != nil {
+			return err
+		}
+	}
+
+	return nil
+}
