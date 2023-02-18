904b87fe745fe9c2bb31d3a0f3a970bd04c77330
logica0419 <84759675+logica0419@users.noreply.github.com>
2023-02-19 03:33:36 +0900

    add timeout error

--- bench/model/errors.go
+++ bench/model/errors.go
@@ -1,6 +1,9 @@
 package model
 
 import (
+	"context"
+	"net"
+
 	"github.com/isucon/isucandar"
 	"github.com/isucon/isucandar/failure"
 )
@@ -17,9 +20,23 @@ var (
 	ErrUndecodableBody failure.StringCode = "undecodable_body"
 	// レスポンスボディが間違っている
 	ErrInvalidBody failure.StringCode = "invalid_body"
+
+	// タイムアウト
+	ErrTimeout failure.StringCode = "timeout"
 )
 
 func IsErrCritical(err error) bool {
 	return failure.IsCode(err, isucandar.ErrPrepare) ||
 		failure.IsCode(err, ErrCritical)
+}
+
+func IsErrTimeout(err error) bool {
+	var nErr net.Error
+	if failure.As(err, &nErr) && nErr.Timeout() {
+		return true
+	}
+	if failure.Is(err, context.DeadlineExceeded) {
+		return true
+	}
+	return failure.IsCode(err, failure.TimeoutErrorCode)
 }
