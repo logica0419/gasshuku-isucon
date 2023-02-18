3822d273a91eeae3f2ca6972a936e3a18860b45d
logica0419 <84759675+logica0419@users.noreply.github.com>
2023-02-19 02:13:57 +0900

    define logger

---
+++ bench/logger/logger.go
@@ -0,0 +1,18 @@
+package logger
+
+import (
+	"log"
+	"os"
+)
+
+var (
+	// 競技者に見せてもいい内容を書くロガー
+	Contestant *log.Logger
+	// 運営だけが見れる内容を書くロガー
+	Admin *log.Logger
+)
+
+func init() {
+	Contestant = log.New(os.Stdout, "", log.Lmicroseconds)
+	Admin = log.New(os.Stderr, "[Admin]", log.Lmicroseconds)
+}
