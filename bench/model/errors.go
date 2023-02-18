6056a839a1e0841069a96e7790a4998d441e645b
logica0419 <84759675+logica0419@users.noreply.github.com>
2023-02-19 02:13:47 +0900

    add error

--- bench/model/errors.go
+++ bench/model/errors.go
@@ -1,6 +1,9 @@
 package model
 
-import "github.com/isucon/isucandar/failure"
+import (
+	"github.com/isucon/isucandar"
+	"github.com/isucon/isucandar/failure"
+)
 
 var (
 	// 予期しないクリティカルなエラー
@@ -8,4 +11,15 @@ var (
 
 	// リクエスト失敗
 	ErrRequestFailed failure.StringCode = "request_failed"
+	// ステータスコードが不正
+	ErrInvalidStatusCode failure.StringCode = "invalid_status_code"
+	// レスポンスボディがデコード不可
+	ErrUndecodableBody failure.StringCode = "undecodable_body"
+	// レスポンスボディが間違っている
+	ErrInvalidBody failure.StringCode = "invalid_body"
 )
+
+func IsErrCritical(err error) bool {
+	return failure.IsCode(err, isucandar.ErrPrepare) ||
+		failure.IsCode(err, ErrCritical)
+}
