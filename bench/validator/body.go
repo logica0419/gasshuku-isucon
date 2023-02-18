50b073b70b6027942dc45525f84bc4d5ffd1a431
logica0419 <84759675+logica0419@users.noreply.github.com>
2023-02-19 03:46:56 +0900

    fix error

--- bench/validator/body.go
+++ bench/validator/body.go
@@ -1,8 +1,11 @@
 package validator
 
 import (
+	"fmt"
 	"net/http"
 
+	"github.com/isucon/isucandar/failure"
+	"github.com/logica0419/gasshuku-isucon/bench/model"
 	"github.com/logica0419/gasshuku-isucon/bench/utils"
 )
 
@@ -10,11 +13,12 @@ func WithBody[V comparable](v V) ValidateOpt {
 	return func(res *http.Response) error {
 		var body V
 		if err := utils.ReaderToStruct(res.Body, &body); err != nil {
-			return nil
+			return failure.NewError(model.ErrUndecodableBody, err)
 		}
 
 		if body != v {
-			return nil
+			return failure.NewError(model.ErrInvalidBody,
+				fmt.Errorf("different body - expected: %v, actual: %v", v, body))
 		}
 		return nil
 	}
