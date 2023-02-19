e6c9032caf68993a5b6909ef8dd2741ec61c3418
logica0419 <84759675+logica0419@users.noreply.github.com>
2023-02-20 02:05:48 +0900

    main with wire

--- bench/main.go
+++ bench/main.go
@@ -2,35 +2,17 @@ package main
 
 import (
 	"context"
-	"log"
 
 	"github.com/isucon/isucandar/worker"
-	"github.com/logica0419/gasshuku-isucon/bench/action"
 	"github.com/logica0419/gasshuku-isucon/bench/benchmark"
-	"github.com/logica0419/gasshuku-isucon/bench/flow"
-	"github.com/logica0419/gasshuku-isucon/bench/scenario"
+	"github.com/logica0419/gasshuku-isucon/bench/logger"
 )
 
 func main() {
-	a, err := action.NewActionController(3, 3, "http://localhost:8080")
+	b, err := benchmark.NewBenchmark(make(chan worker.WorkerFunc, 100))
 	if err != nil {
-		log.Panic(err)
+		logger.Admin.Panic(err)
 	}
 
-	c := make(chan worker.WorkerFunc, 100)
-
-	f, err := flow.NewFlowController(c, a, a)
-	if err != nil {
-		log.Panic(err)
-	}
-
-	s := scenario.NewScenario(c, f)
-
-	b, err := benchmark.NewBenchmark(s)
-	if err != nil {
-		log.Panic(err)
-	}
-
-	ctx := context.Background()
-	b.Run(ctx)
+	b.Run(context.Background())
 }
