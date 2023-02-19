085313b229b5e763b40f674dcc07d1260fa315ee
logica0419 <84759675+logica0419@users.noreply.github.com>
2023-02-19 11:28:57 +0900

    fix json validate function

--- bench/validator/json.go
+++ bench/validator/json.go
@@ -9,13 +9,26 @@ import (
 	"github.com/logica0419/gasshuku-isucon/bench/utils"
 )
 
-func WithBody[V comparable](v V) ValidateOpt {
+type JsonValidateOpt[V comparable] func(body V) error
+
+func WithBodyValidation[V comparable](opt ...JsonValidateOpt[V]) ValidateOpt {
 	return func(res *http.Response) error {
 		var body V
 		if err := utils.ReaderToStruct(res.Body, &body); err != nil {
 			return failure.NewError(model.ErrUndecodableBody, err)
 		}
 
+		for _, o := range opt {
+			if err := o(body); err != nil {
+				return err
+			}
+		}
+		return nil
+	}
+}
+
+func WithBodyEquals[V comparable](v V) JsonValidateOpt[V] {
+	return func(body V) error {
 		if body != v {
 			return failure.NewError(model.ErrInvalidBody,
 				fmt.Errorf("different body - expected: %v, actual: %v", v, body))
