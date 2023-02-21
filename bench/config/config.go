055dc76a778adc8a269ff0974aa17c04b4134383
logica0419 <84759675+logica0419@users.noreply.github.com>
2023-02-22 03:47:03 +0900

    fix flag name

--- bench/config/config.go
+++ bench/config/config.go
@@ -6,17 +6,17 @@ import (
 )
 
 type Config struct {
-	BaseURL           string `mapstructure:"base_url"`            // ベンチマーク先のURL
-	RequestTimeout    int    `mapstructure:"request_timeout"`     // リクエストタイムアウト時間(ミリ秒)
-	InitializeTimeout int    `mapstructure:"initialize_timeout"`  // 初期化タイムアウト時間(ミリ秒)
-	ExitStatusOnFail  bool   `mapstructure:"exit_status_on_fail"` // ベンチマーク失敗時にexit statusを1にするかどうか
+	BaseURL           string `mapstructure:"base-url"`           // ベンチマーク先のURL
+	RequestTimeout    int    `mapstructure:"request-timeout"`    // リクエストタイムアウト時間(ミリ秒)
+	InitializeTimeout int    `mapstructure:"initialize-timeout"` // 初期化タイムアウト時間(ミリ秒)
+	ExitStatusOnFail  bool   `mapstructure:"exit-status"`        // ベンチマーク失敗時にexit statusを1にするかどうか
 }
 
 func init() {
-	pflag.String("base_url", "http://localhost:8080", "ベンチマーク先のURL")
-	pflag.Int("request_timeout", 1000, "リクエストタイムアウト時間(ミリ秒)")
-	pflag.Int("initialize_timeout", 30000, "初期化タイムアウト時間(ミリ秒)")
-	pflag.Bool("exit_status_on_fail", false, "ベンチマーク失敗時にexit statusを1にするかどうか")
+	pflag.String("base-url", "http://localhost:8080", "ベンチマーク先のURL")
+	pflag.Int("request-timeout", 1000, "リクエストタイムアウト時間(ミリ秒)")
+	pflag.Int("initialize-timeout", 30000, "初期化タイムアウト時間(ミリ秒)")
+	pflag.Bool("exit-status", false, "ベンチマーク失敗時にexit statusを1にするかどうか")
 }
 
 func NewConfig() (c *Config, err error) {
