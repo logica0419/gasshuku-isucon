916418a86f28b23986d2302e89aab6c8aeb84ab5
logica0419 <84759675+logica0419@users.noreply.github.com>
2023-02-24 01:04:07 +0900

    rethought score

--- bench/grader/tag.go
+++ bench/grader/tag.go
@@ -20,15 +20,15 @@ var (
 )
 
 func setScore(result *isucandar.BenchmarkResult) {
-	result.Score.Set(ScorePostMember, 20)
-	result.Score.Set(ScoreGetMembers, 15)
+	result.Score.Set(ScorePostMember, 15)
+	result.Score.Set(ScoreGetMembers, 10)
 	result.Score.Set(ScoreBanMember, 1)
-	result.Score.Set(ScoreUpdateMember, 5)
+	result.Score.Set(ScoreUpdateMember, 3)
 
-	result.Score.Set(ScorePostBooks, 20)
-	result.Score.Set(ScoreSearchBooks, 15)
+	result.Score.Set(ScorePostBooks, 15)
+	result.Score.Set(ScoreSearchBooks, 10)
 
-	result.Score.Set(ScorePostLendings, 25)
-	result.Score.Set(ScoreGetLendings, 15)
-	result.Score.Set(ScoreReturnLendings, 25)
+	result.Score.Set(ScorePostLendings, 20)
+	result.Score.Set(ScoreGetLendings, 10)
+	result.Score.Set(ScoreReturnLendings, 20)
 }
